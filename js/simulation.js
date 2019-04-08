549cfed24b227d6719c727f67649707945f91aec
jDeerin-hackGT <2016deerinjo@gmail.com>
2019-04-08 14:20:23 -0400

    Minor refactoring

--- js/simulation.js
+++ js/simulation.js
@@ -1,11 +1,18 @@
 // TODO: Actually implement the simulation, instead of a test of the canvas
 
-canvas = document.getElementById('sim-canvas');
-updateCanvasSize(canvas);
-ctx = canvas.getContext('2d');
+drawPlaceholderMessage();
 
-ctx.fillStyle = 'green';
-ctx.fillRect(0, 0, canvas.width, canvas.height)
+function drawPlaceholderMessage() {
+    canvas = document.getElementById('sim-canvas');
+    updateCanvasSize(canvas);
+    ctx = canvas.getContext('2d');
+
+    ctx.fillStyle = 'green';
+    ctx.fillRect(0, 0, canvas.width, canvas.height)
+    ctx.fillStyle = 'white';
+    ctx.font = '24px serif';
+    ctx.fillText('(Imagine Awesome Things Here)', canvas.width/2 - 150, canvas.height/2);
+}
 
 function updateCanvasSize(canvas) {
     canvas.width = canvas.clientWidth;
