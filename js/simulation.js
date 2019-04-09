b5e16ec698dc30d773a82b362329753d6cdf9fcd
jDeerin-hackGT <2016deerinjo@gmail.com>
2019-04-09 10:48:14 -0400

    Added function for spawning 'n' uniformly-random objects to display simulation

--- js/simulation.js
+++ js/simulation.js
@@ -3,18 +3,40 @@
 import { initCanvas, renderSimulation } from './drawing.js';
 import { GravityCalculator, GravityObject, Point3D } from './simulationClasses.js';
 
-//==============================================================================
-
 // TODO: Refactor this/expose it all to the UI
 let simulation = null;
 
-//drawPlaceholderMessage();
 initSimulation();
 runSimulation();
 
 function initSimulation() {
     initCanvas();
 
+    let objects = spawnRandomObjects(10);
+    //objects = spawnTrinarySystem();
+    simulation = new GravityCalculator(objects, 6.6738e-11, 1.0);
+}
+
+function spawnRandomObjects(numObjects) {
+    let objects = [];
+    function randRange(min, max) {
+        return Math.random()*(max - min) + min;
+    }
+
+    for (let i = 0; i < numObjects; i++) {
+        let position = new Point3D(randRange(-1.5, 1.5),
+                                   randRange(-1.5, 1.5),
+                                   randRange(-1.5, 1.5));
+        let velocity = new Point3D(randRange(-0.05, 0.05),
+                                   randRange(-0.05, 0.05),
+                                   randRange(-0.05, 0.05));
+        let mass = randRange(1.0e7, 5.0e7);
+        objects.push(new GravityObject(position, velocity, mass));
+    }
+    return objects;
+}
+
+function spawnTrinarySystem() {
     let objects = [];
     objects.push(new GravityObject(new Point3D(1.5, 0.0, 1.0),
                                    new Point3D(0.0, 0.05, 0.0),
@@ -25,7 +47,7 @@ function initSimulation() {
     objects.push(new GravityObject(new Point3D(0.0, 0.0, 0.0),
                                    new Point3D(0.0, 0.0, 0.0),
                                    1.25e8));
-    simulation = new GravityCalculator(objects, 6.6738e-11, 1.0);
+    return objects;
 }
 
 function runSimulation(timePassed = 0) {
